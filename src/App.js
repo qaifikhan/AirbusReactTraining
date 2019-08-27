552fcf029f8a11855d60e1c4b07cabca0179c8b8
Qaifi Khan <qaifikhan@gmail.com>
2019-08-27 15:08:15 +0530

    component lifecycles

--- src/App.js
+++ src/App.js
@@ -1,98 +1,136 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Comment from './Comment';
 
-import logo from './logo.svg';
 import './App.css';
 
-let commenList = [
-  {
-    profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-    username: 'Username1',
-    message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
-    replies: [
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply1',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply2',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply3',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-    ]
-  },
-  {
-    profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-    username: 'Username2',
-    message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
-    replies: [
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply1',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply2',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply3',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-    ]
-  },
-  {
-    profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-    username: 'Username3',
-    message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
-    replies: [
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply1',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply2',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-      {
-        profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
-        username: 'Reply3',
-        message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
-      },
-    ]
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      showList: true,
+      commenList: [
+        {
+          profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+          username: 'Username1',
+          message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+          showReplies: false,
+          replies: [
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply1',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply2',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply3',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+          ]
+        },
+        {
+          profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+          username: 'Username2',
+          message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+          showReplies: false,
+          replies: [
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply1',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply2',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply3',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+          ]
+        },
+        {
+          profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+          username: 'Username3',
+          message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor',
+          showReplies: false,
+          replies: [
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply1',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply2',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+            {
+              profilePic: 'https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1',
+              username: 'Reply3',
+              message: 'Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor Lorem Ipsum Dolor'
+            },
+          ]
+        }
+      ]
+    }  
+    console.log('Constructor');
   }
-]
 
-function App() {
-  let heading = "Blog 23 Comments"
+ 
+  heading = "Blog 23 Comments"
 
-  let commList = commenList.map((item, commentKey) => {
-    // var replies = item.replies.map((replyItem, pos) => {
-    //   return(
-    //     <Comment data={replyItem} position={pos} name="Arshad" />
-    //   )
-    // })
-    return <Comment data={item} position={commentKey} />
-  })
+  onBtnClick = () => {
+    let updatedVal = !this.state.showList;
+    this.setState({showList: updatedVal});
+  }
 
-  return (
-    <div className="App">
-      <h1>{heading}</h1>
-      
-      {commList}
-    </div>
-  );
+  onReplyBtnClick = (pos) => {
+    let updatedArrayList = [...this.state.commenList];
+    let updatedCommentObj = updatedArrayList[pos];
+    let updatedReplyStatus = !updatedCommentObj.showReplies;
+    updatedCommentObj.showReplies = updatedReplyStatus;
+    updatedArrayList[pos] = updatedCommentObj;
+
+    this.setState({commenList: updatedArrayList});
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps);
+    console.log(nextState);
+    console.log('Component Should Update')
+    return true;
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+  }
+
+  componentDidUpdate() {
+    console.log('Component Did Update');
+  }
+
+  render() {
+    console.log('Render');
+    let commList = this.state.commenList.map((item, commentKey) => {
+      return <Comment data={item} pos={commentKey} key={commentKey} btnClick={() => this.onReplyBtnClick(commentKey)}
+       />
+    })
+
+    return (
+      <div className="App">
+        <h1>{this.heading}</h1>
+        {this.state.showList ? commList : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Comment.js
+++ src/Comment.js
@@ -3,14 +3,25 @@ import React from 'react';
 import classes from './Comment.module.css';
 
 var Comment = (props) => {
-    console.log('CommentItem Props => ', props);
+    if(props.reply) {
+        console.log('Reply Component')
+    } else {
+        console.log('Comment Component')
+    }
+    var replyList = props.data.replies !== undefined ? props.data.replies.map((replyItem, pos) => {
+        return(
+            <Comment data={replyItem} key={pos} reply={true} />
+        )
+    }) : null;
     return (
-        <div className={classes.card}>
+        <div className={props.reply ? classes.ReplyCard : classes.card}>
             <img src={props.data.profilePic} alt={'Profile Pic'} />
             <div>
             <h3>{props.data.username}</h3>
             <p>{props.data.message}</p>
-            {/* {replies} */}
+            {props.reply ? null : <button onClick={props.btnClick} className={classes.ShowHideBtn}>Show/Hide Replies</button>}
+
+            {props.data.showReplies ? replyList: null}
             </div>
         </div>
     )
